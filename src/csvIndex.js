62be3404e2c0e05e247f481c6e8968f04139bdf3
Sushmey <sushmeywork@gmail.com>
2022-02-01 18:51:55 +0530

    Initial commit

---
+++ src/csvIndex.js
@@ -0,0 +1,32 @@
+// var fs = require('fs');
+var wordList = require('wordlist-english'); //This library contains words arranged according to the word length
+
+var totalWords =[];
+var words = wordList['english/american'];
+totalWords.push(...words); //spread operator to handle multiple elements
+// words = wordList['english/american/20'];
+// totalWords.push(...words); //spread operator to handle multiple elements
+// words = wordList['english/american/35'];
+// totalWords.push(...words); //spread operator to handle multiple elements
+// words = wordList['english/american/40'];
+// totalWords.push(...words); //spread operator to handle multiple elements
+// words = wordList['english/american/50'];
+// totalWords.push(...words); //spread operator to handle multiple elements
+// words = wordList['english/american/55'];
+// totalWords.push(...words); //spread operator to handle multiple elements
+// words = wordList['english/american/60'];
+totalWords.push(...words); //spread operator to handle multiple elements
+// for(let i=100;i<110;i++)
+// {
+// 	console.log(words[i]);
+// }
+console.log(totalWords.length);
+
+// // console.log(letterArray);
+const wordsOfFive = totalWords.filter((word)=>{return word.length==5}); //Filtering the array to keep only words of length 5
+var letterArray = wordsOfFive.map((word)=>{return word.split('')}); //Converting the wordList string array to an array of characters of each word
+console.log(letterArray.length);
+for(let i=0;i<wordsOfFive.length;i++)
+{
+	console.log(letterArray[i]);
+}
---
+++ src/index.js
@@ -0,0 +1,95 @@
+// var fs = require('fs');
+var wordList = require('word-list-json'); //This library contains words arranged according to the word length
+
+var letterArray = wordList.map((word)=>{return word.split('')}); //Converting the wordList string array to an array of characters of each word
+// console.log(letterArray);
+const wordsOfFive = letterArray.filter((letter)=>{return letter.length==5}); //Filtering the array to keep only words of length 5
+//console.log(wordsOfFive);
+
+//var guess = ['t','h','i'];
+
+// export { wordsOfFive };
+const isSubset= (word, guess)=>{
+  // returns true if guess is a subset of word
+  return guess.every((element)=>{
+    return word.includes(element);
+  });
+}
+
+//We need a function which checks if guess is a subset of the wordsOfFive but we also need to maintain the order
+//We'll just join the char array to a string and then use includes function to check if it contains
+
+const isSubsetOrdered= (wordArray, guess)=>{
+  // returns true if guess is a subset of word
+  const word = wordArray.filter((word) => {word.trim().length>0}); //removing all spaces
+  return guess.every(()=>{
+    return word.join('').includes(guess.join(''));
+  });
+}
+
+
+const containsLetter= (word, guess)=>{
+  // returns true if word contains any letter from excluded letters
+  return guess.some((element)=>{
+    return word.includes(element);
+  });
+}
+
+
+
+const suggestionsDiv = document.getElementById("suggestions"); //Getting the div from html where we will display the words 
+document.guessFunction = ()=>{
+	suggestionsDiv.innerHTML = ""; //Clearing the div
+	document.getElementById("footer").classList.remove("fixMe"); //Cosmetic
+	// document.getElementById("deleteMe#2").classList.remove("hiddenBox"); //Cosmetic
+	var ctr=0;//Counter to keep track of how many words are displayed so far
+	var exclude = document.getElementById("exclude").value.toLowerCase().trim();
+	var guessedWord = document.getElementById("guess").value.toLowerCase().trim(); //Taking input from the html
+	suggestionsDiv.classList.add("border","border-1","dotted","boxy"); //Adding classes dynamically
+	if(guessedWord.length==0) //validation to check if empty 
+	{
+		suggestionsDiv.append(document.createTextNode("Add some letters! \n"));
+	}
+	else
+	{
+		var guess = guessedWord.split(''); //Splitting the word to char array
+		var excluded =exclude.length==0?['/']:exclude.split(''); //If empty then adding an array that will never be a subset
+		//var excluded = exclude.split('');
+		for(let i =0;i<wordsOfFive.length;i++)
+		{
+			if(isSubset(wordsOfFive[i], guess) && !containsLetter(wordsOfFive[i],excluded)) 
+			{
+					suggestionsDiv.append(document.createTextNode(wordsOfFive[i].join('')+"\n"));
+					ctr++; //Counter to keep track of how many words are displayed so far
+					//console.log(isSubset(wordsOfFive[i],excluded));
+					//console.log(isSubsetOrdered(wordsOfFive[i],guess)?wordsOfFive[i]:isSubsetOrdered(wordsOfFive[i],guess));
+					//console.log(wordsOfFive[i]);
+			}
+		}
+		if(ctr==0) //To show something when there are no words found
+		{
+			suggestionsDiv.append(document.createTextNode("No words found :(\n"));
+		}
+	}
+};
+
+
+
+
+
+
+
+/*
+TODO:
+Trim inputs - DONE
+Validation to check if guess text is empty - DONE
+Validation to check if exclude text is empty - DONE
+Something to display if no words found - DONE
+Validation for checking if input contains anything other than words - It anyway gives false so no need. - DONE
+Validation to check that guess text length isn't greater than 5 - No need since isSubset still works - DONE
+
+Add way to input ordered letters so suggested words are more accurate
+*/
+
+
+
